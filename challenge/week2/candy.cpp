a804bacb94eb5e831ab5f534ea2df595b1f122bc
YIFNEN <lideram@naver.com>
2024-09-11 16:43:55 +0900

    Challenge_candy, Challenge_temperature

--- challenge/week2/candy.cpp
+++ challenge/week2/candy.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
+#include <string>
+#include <cmath>
 using namespace std;
 
 int main()
-{
-  
+{ 
+    float money;
+    cout << "가진 돈은? ";
+    cin >> money;
+    float price;
+    cout << "사탕 가격은? ";
+    cin >> price;
+    float candynum, charge;
+    candynum = floor (money / price);
+    charge = money - candynum * price;
+    
+    cout << "현재 가지고 있는 돈:"<< money << endl;
+    cout <<"캔디의 가격"<< price << endl;
+    cout <<"최대로 살 수 있는 캔디"<< candynum << endl;
+    cout <<"캔디 구입 후 남은 돈"<< charge << endl;
 }
--- challenge/week2/temperature.cpp
+++ challenge/week2/temperature.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int x = 1;
-    cout << "x = "<< x++ << endl;
-    cout << "x = "<< x++ << endl;
-    cout << "x = "<< ++x << endl;
-    cout << "x = "<< x-- << endl;
-    cout << "x = "<< x-- << endl;
-    cout << "x = "<< --x << endl;
-    
+    float Ftemp = 0.0;
+    float Ctemp = 0.0;
+    cout << "화씨 온도: ";
+    cin >> Ftemp;
+    Ctemp = (5.0 / 9.0) * (Ftemp-32.0);
+
+    cout << "섭씨 온도:"<< Ctemp << endl;
     return 0;
-}
+}3333
